e0d6570cd8c397c8bbc7e51da36540452e4e4db6
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2023-09-22 19:04:38 -0400

    adding methods to store

--- src/stores/CardStore.js
+++ src/stores/CardStore.js
@@ -3,11 +3,36 @@ import ApiService from '@/services/Api.js';
 
 export const useCardStore = defineStore('card', {
   state: () => ({
-    cardList: [],
+    allCards: [],
+    selectedCards: [],
   }),
   actions: {
     getCardList() {
-      this.cardList = ApiService.getCardList();
+      this.allCards = ApiService.getCardList();
+    },
+    getLocalData() {
+      const localData = localStorage.getItem('sveProxies');
+      if (localData) {
+        this.selectedCards = JSON.parse(localData);
+      }
+    },
+    saveLocalData() {
+      const stringifyData = JSON.stringify(this.selectedCards);
+      localStorage.setItem('sveProxies', stringifyData);
+    },
+    addCard(card) {
+      this.selectedCards.push(card);
+      this.selectedCards.sort((a, b) => a.name.localeCompare(b.name) || a.id - b.id);
+      this.saveLocalData();
+    },
+    removeCard(id) {
+      const index = this.selectedCards.findIndex(item => item.id === id);
+      this.selectedCards.splice(index, 1);
+      this.saveLocalData();
+    },
+    removeAllCards() {
+      this.selectedCards = [];
+      this.saveLocalData();
     },
   },
 });
