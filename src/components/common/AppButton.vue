65aa70f8481ebfd2bc88885b351b70b01251705b
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2023-10-03 14:23:49 -0400

    adding button component

---
+++ src/components/common/AppButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    class="btn"
+    :class="{ 'icon': isIcon }"
+    :disabled="isDisabled"
+    type="button"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+  props: {
+    isDisabled: {
+      default: false,
+      required: false,
+      type: Boolean,
+    },
+    isIcon: {
+      default: false,
+      required: false,
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.btn {
+  background-color: var(--color-black);
+  color: var(--color-white);
+  border-radius: 0.25rem;
+  border: none;
+  padding: 0.375rem 0.75rem;
+  transition: 0.2s;
+  cursor: pointer;
+  font-size: 1rem;
+  line-height: 1;
+
+  &:hover,
+  &:focus {
+    background-color: var(--color-dark-gray);
+  }
+}
+</style>
