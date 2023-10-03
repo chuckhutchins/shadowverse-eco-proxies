5ee9d170bf493412c7697d97da820bef0da0c3a4
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2023-10-03 14:24:10 -0400

    adding help dialog to header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,68 +4,53 @@
       <h1>Shadowverse Evolve Eco Proxies</h1>
       <ul class="nav-items">
         <li>
-          <button @click="printCards" type="button">
+          <AppButton @click="printCards">
             Print
-          </button>
+          </AppButton>
         </li>
         <li>
-          <button @click="openHelpDialog" type="button">
+          <AppButton @click="openHelpDialog">
             Help
-          </button>
+          </AppButton>
         </li>
       </ul>
     </nav>
   </header>
-  <dialog
-    ref="dialog"
-    class="dialog"
-    inert
-    @click:outside="closeHelpDialog"
-    @keydown.esc="closeHelpDialog"
-  >
-    <div class="header">
+  <AppDialog ref="helpDialog">
+    <template v-slot:header>
       <h2>Help</h2>
-    </div>
-    <button
-      ref="closeBtn"
-      class="btn-close"
-      type="button"
-      @click="closeHelpDialog"
-    >
-      <IconClose />
-      <span class="sr-only">Close help dialog</span>
-    </button>
-    <div class="content">
-      asdf
-    </div>
-  </dialog>
+    </template>
+    <h3>Recommended Settings</h3>
+    <p>You should be fine switching between US Letter and A4. Folks with A4 paper will have to let me know if that's not the case.</p>
+    <ul>
+      <li>Orientation: Portrait</li>
+      <li>Scale: 100%</li>
+      <li>Margins: Minimum</li>
+    </ul>
+    <p>If you check the "Print backgrounds" setting, the box around the "Token" trait gets filled in.</p>
+    <h3>Issues</h3>
+    <p>Do I have a typo somewhere in the card data? Do you want me to add specific cards? Is something just not working? Please create an issue in the <a href="https://github.com/chuckhutchins/shadowverse-eco-proxies/issues" target="_blank">Issues tab</a>.</p>
+    <h3>Support</h3>
+    <p>This is totally not necessary, but if you'd like to, I have a <a href="https://ko-fi.com/brokendivsociety" target="_blank">Ko-Fi</a> you can toss a dollar or two at if you like the application.</p>
+  </AppDialog>
 </template>
 
 <script>
-import IconClose from '@/components/icons/IconClose.vue';
+import AppButton from '@/components/common/AppButton.vue';
+import AppDialog from '@/components/common/AppDialog.vue';
 
 export default {
   name: 'TheHeader',
   components: {
-    IconClose,
+    AppButton,
+    AppDialog,
   },
   methods: {
     openHelpDialog() {
-      // TODO: open help dialog
-      // TODO: do i allow borders to be toggled on/off? and maybe just do dots?
-      // TODO: give instructions on the filled in or outlined token box
-      console.log('opening help dialog');
-      this.$refs.dialog.removeAttribute('inert');
-      this.$refs.dialog.showModal();
-    },
-    closeHelpDialog() {
-      console.log('closing help dialog');
-      this.$refs.dialog.close();
-      this.$refs.dialog.setAttribute('inert', '');
+      this.$refs.helpDialog.openDialog();
     },
     printCards() {
-      // TODO: trigger system print dialog
-      console.log('triggering print dialog');
+      window.print();
     },
   }
 };
@@ -79,88 +64,15 @@ export default {
   .nav {
     display: flex;
     gap: 2rem;
-    flex-direction: column;
     justify-content: space-between;
     align-items: center;
   }
 
   .nav-items {
-    // TODO: style navbar
-    display: none;
-  }
-}
-
-.dialog {
-  position: fixed;
-  inset: 0;
-  margin: auto;
-  display: grid;
-  grid-template-rows: minmax(1.5rem, 1fr);
-  grid-template-columns: 1fr 1rem;
-  gap: 1rem 0.5rem;
-  padding: 1rem;
-  background-color: var(--color-white);
-  border: none;
-  box-shadow: var(--card-box-shadow);
-  border-radius: 0.5rem;
-  inline-size: 30rem;
-  max-inline-size: min(90vw, 30rem);
-  max-block-size: min(80vh, 100%);
-  max-block-size: min(80dvb, 100%);
-  overflow: hidden;
-  transition: opacity .2s ease;
-
-  @media (min-width: 48rem) {
-    padding: 1.5rem;
-    gap: 1.5rem 0.5rem;
-  }
-
-  > * {
-    min-inline-size: 0;
-  }
-
-  &:not([open]) {
-    pointer-events: none;
-    opacity: 0;
-  }
-
-  &::backdrop {
-    background-color: rgba(0, 0, 0, 0.5);
-    backdrop-filter: blur(0.5rem);
-  }
-
-  .btn-close {
-    position: absolute;
-    inset-block-start: 0.5rem;
-    inset-inline-end: 0.5rem;
-    background-color: transparent;
-    border-radius: 50%;
-    border: none;
-    padding: 0.25rem;
-    transition: 0.2s;
-    aspect-ratio: 1;
-    cursor: pointer;
-    line-height: 0;
-
-    svg {
-      aspect-ratio: 1;
-      inline-size: 2rem;
-      transition: .2s;
-    }
-
-    &:hover,
-    &:focus {
-      background-color: var(--color-light-gray);
-
-      svg {
-        transform: rotate(90deg);
-      }
-    }
-  }
-
-  .content,
-  .footer {
-    grid-column: span 2;
+    display: flex;
+    gap: 1rem;
+    list-style-type: none;
+    padding-left: 0;
   }
 }
 
